0aeaebfb037a1519abb93add6b7a6bfbef845254
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-13 23:59:41 -0500

    refactor route specialty

--- controllers/specialty.js
+++ controllers/specialty.js
@@ -11,11 +11,48 @@ module.exports = {
         }
     },
 
-    newSpecialty: async (req, res, next) =>{
+    newSpecialty: async (req, res, next) => {
         const newSpecialty = new Specialty(req.body)
         const specialty = await newSpecialty.save();
         try {
             res.status(201).json(specialty);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    },
+
+    showSpecialty: async (req, res, next) => {
+
+        const { specialtyId } = req.params;
+        const specialty = await Specialty.findOne(specialtyId)
+
+        try {
+            res.json(specialty);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    },
+
+    updateSpecialty: async (req, res, next) => {
+
+        const { specialtyId } = req.params;
+        const newSpecialty = new Specialty(req.body);
+        const result = await Specialty.findByIdAndUpdate(specialtyId, newSpecialty);
+
+        try {
+            res.json(newSpecialty);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    },
+
+    removeSpecialty: async (req, res, next) => {
+
+        const { specialtyId } = req.params;
+        const specialty = await Specialty.findOneAndRemove(specialtyId);
+
+        try {
+            res.json({ message: "Deleted Specialty" });
         } catch (err) {
             res.status(400).json({ message: err.message });
         }
