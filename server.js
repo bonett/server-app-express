943df08d6f99aafa0db5278f0cb0ca5cf6070070
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-13 20:07:42 -0500

    Add files config and mongoose

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', () => console.log(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.listen(8000, () => console.log('server started on port'))
