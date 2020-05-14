69be37bbc36d0a5cd3f5248852e956e78366a65a
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-14 00:59:33 -0500

    create method show and create

--- controllers/provider.js
+++ controllers/provider.js
@@ -4,7 +4,9 @@ const Specialty = require('../models/specialty');
 module.exports = {
 
     index: async (req, res, next) => {
+
         const Providers = await Provider.find();
+
         try {
             res.json(Providers);
         } catch (err) {
@@ -13,10 +15,31 @@ module.exports = {
     },
 
     newProvider: async (req, res, next) => {
-        const newSpecialty = new Specialty(req.body)
-        const specialty = await newSpecialty.save();
+
+        const params = req.body;
+        const specialtyId = params.specialty;
+        const getSpecialty = await Specialty.findById(specialtyId);
+
+        const newProvider = new Provider({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            middleName: req.body.middleName,
+            email: req.body.email,
+            specialty: getSpecialty,
+            projectedStartDate: req.body.projectedStartDate,
+            employerId: req.body.employerId,
+            providerType: req.body.providerType,
+            staffStatus: req.body.staffStatus,
+            status: req.body.status,
+            createdBy: req.body.createdBy,
+            updatedBy: req.body.updatedBy,
+            photo: req.body.photo
+        });
+
+        await newProvider.save();
+
         try {
-            res.status(201).json(specialty);
+            res.status(201).json(newProvider);
         } catch (err) {
             res.status(400).json({ message: err.message });
         }
@@ -25,13 +48,31 @@ module.exports = {
     showProvider: async (req, res, next) => {
 
         const { providerId } = req.params;
-        const provider = await Provider.findById(providerId)
+        const provider = await Provider.findById(providerId);
+        const specialtyId = provider.specialty;
+        const getSpecialty = await Specialty.findById(specialtyId);
+
+        const newProvider = new Provider({
+            firstName: provider.firstName,
+            lastName: provider.lastName,
+            middleName: provider.middleName,
+            email: provider.email,
+            specialty: getSpecialty,
+            projectedStartDate: provider.projectedStartDate,
+            employerId: provider.employerId,
+            providerType: provider.providerType,
+            staffStatus: provider.staffStatus,
+            status: provider.status,
+            createdBy: provider.createdBy,
+            updatedBy: provider.updatedBy,
+            photo: provider.photo
+        });
 
         try {
             if (provider == null) {
                 return res.status(404).json({ message: 'Cannot find provider' });
             } else {
-                res.json(provider);
+                res.json(newProvider);
             }
         } catch (err) {
             return res.status(500).json({ message: err.message });
@@ -54,7 +95,6 @@ module.exports = {
     removeProvider: async (req, res, next) => {
 
         const { providerId } = req.params;
-
         const provider = await Provider.findById(providerId);
 
         try {
